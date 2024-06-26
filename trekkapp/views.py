f62ca7eab0e1a338f96855b73b73f0c3850ebcaf
Jaideep24 <vinnijaideep@gmail.com>
2024-06-26 18:00:04 +0530

    Fourth commit

--- trekkapp/views.py
+++ trekkapp/views.py
@@ -5,12 +5,13 @@ from django.views.generic import ListView, DetailView, DeleteView, TemplateView,
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+import json
 #from .forms import *
 from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request,'trekkapp/trekking.html', {"name":"Monsson", "trekking":Trekking.objects.all(),"cities":City.objects.all(),"festival":Festival.objects.all(),"adventure":Adventure.objects.all(), "camping":Camping.objects.all(),"cycling":Cycling.objects.all(), "tours":Tours.objects.all() })
+    return render(request,'trekkapp/trekking.html', {"name":"Monsson", "trekking":Trekking.objects.all(),"cities":City.objects.all(),"festival":Festival.objects.all(),"adventure":Adventure.objects.all(), "camping":Camping.objects.all(),"cycling":Cycling.objects.all(), "tours":Tours.objects.all(),"cities_json":json.dumps(list(City.objects.all().values('name'))) })
 
 class Detailedtrek(DetailView):
     model=City
